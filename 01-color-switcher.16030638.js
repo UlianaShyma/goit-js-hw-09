ac39b1596602e93590bf9ddb74eecf674c79855d
UlianaShyma <90284918+UlianaShyma@users.noreply.github.com>
2023-01-10 18:48:47 +0000

    Deploying to gh-pages from @ UlianaShyma/goit-js-hw-09@069ba33ffaa8b3fa4700873ba6a25e192f6d0e92 🚀

---
+++ 01-color-switcher.16030638.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]");
+//# sourceMappingURL=01-color-switcher.16030638.js.map
---
+++ 01-color-switcher.3dc945b3.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]");
+//# sourceMappingURL=01-color-switcher.3dc945b3.js.map
